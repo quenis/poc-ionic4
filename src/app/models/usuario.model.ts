1088fea7970f4baa2421bab8376cf7bc8dfc599a
Quenis Chagas <quenis@hotmail.com.br>
2019-09-12 11:02:43 -0300

    .

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,6 @@
+export class Usuario {
+    nome: string;
+    matricula: string;  
+    assinatura: string; // Token JWT usado para consultar informações pessoais do usuário logado 
+    login: string; 
+}
