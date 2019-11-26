a1f26c93d1d7e8b72e8a075c5199c870bc32534b
Andre Luis <aisitiger.98@gmail.com>
2019-11-26 12:10:59 -0300

    implementando popup ao clicar em um ponto

--- src/app/services/mapbox.service.ts
+++ src/app/services/mapbox.service.ts
@@ -11,10 +11,10 @@ export class MapboxService {
   private _mapInstance: any;
 
   private manyPoints: any[] = [
-    { latitude: -16.676566, longitude: -49.257486 },
-    { latitude: -16.674840, longitude: -49.256273 },
-    { latitude: -16.676093, longitude: -49.253934 },
-    { latitude: -16.683216, longitude: -49.257829 }
+    { latitude: -16.676566, longitude: -49.257486, titulo: 'Mecânica', descricao: 'Oficina de conserto de bicicleta motorizada, produzida na china no ano de 1810.' },
+    { latitude: -16.674840, longitude: -49.256273, titulo: 'Padaria', descricao: 'Padaria do seu zé, referência em como não fazer pão a mais de 200 anos em Goiânia.' },
+    { latitude: -16.676093, longitude: -49.253934, titulo: "Bob's", descricao: 'Um salão tradicional da região inspirado na mulher mais ilustre de todas Dona Florinda.' },
+    { latitude: -16.683216, longitude: -49.257829, titulo: 'Faculdade BySelf', descricao: 'Ensino modelo mundial onde o aluno decide o que estudar e que horas estudar.' }
   ]
 
   constructor(
@@ -39,8 +39,12 @@ export class MapboxService {
   }
 
   public markerManyPoints() {
-    this.manyPoints.forEach(coordinate => {
-      new mapbox.Marker({ color: '#1F75FE' }).setLngLat([coordinate.longitude, coordinate.latitude]).addTo(this._mapInstance);
+    this.manyPoints.forEach(point => {
+      new mapbox.Marker({ color: '#1F75FE' }).setLngLat([point.longitude, point.latitude])
+      .setPopup(new mapbox.Popup({ offset: 25 }).setHTML('<h3>' + point.titulo + '</h3><p>' + point.descricao + '</p>'))
+      .addTo(this._mapInstance);
     });
   }
+
+
 }
